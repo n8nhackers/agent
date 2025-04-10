3c6b9f0e04a0464b6c9dde6364291c0713fcccd1
Miquel Colomer <mcolomer@gmail.com>
2025-04-10 21:43:45 +0200

    Add new methods for agent

--- agent.py
+++ agent.py
@@ -6,106 +6,113 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-INSTANCES = os.getenv("INSTANCES").split(';')  # Split the INSTANCES by semicolon to get each instance
-# print(f"Loaded Instances: {INSTANCES}")
+N8NHACKERS_API_KEY = os.getenv("N8NHACKERS_API_KEY")
+N8NHACKERS_API_URL = os.getenv("N8NHACKERS_API_URL")
 
+# Dynamically load instances from environment variables
+INSTANCES = []
+for i in range(1, 11):  # Assuming up to 10 instances
+    instance_name = os.getenv(f"INSTANCE_NAME{i}")
+    instance_url = os.getenv(f"INSTANCE_URL{i}")
+    instance_api_key = os.getenv(f"INSTANCE_API_KEY{i}")
+    if instance_name and instance_url and instance_api_key:
+        INSTANCES.append((instance_name, instance_url, instance_api_key))
+        
 def get_executions_from_n8n(instance_url, api_key):
     headers = {'Authorization': f'Bearer {api_key}'}
     try:
-        response = requests.get(f'{instance_url}/api/v1/executions?limit=1000', headers=headers)
+        final_url = f'{instance_url}/api/v1/executions?limit=1000'
+        response = requests.get(final_url, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching executions from {instance_url}: {e}")
         return None
-    
+
 def get_workflows_from_n8n(instance_url, api_key):
     headers = {'Authorization': f'Bearer {api_key}'}
     try:
-        response = requests.get(f'{instance_url}/api/v1/workflows?limit=1000', headers=headers)
+        final_url = f'{instance_url}/api/v1/workflows?limit=1000'
+        response = requests.get(final_url, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching workflows from {instance_url}: {e}")
+        print(f"Error fetching executions from {instance_url}: {e}")
         return None
+
+def check_up(instance_url, api_key):
+    #check if instance_url is up
+    try:
+        response = requests.get(instance_url)
+        return response.ok
+    except Exception as e:
+        return False
     
-def get_audit_from_n8n(instance_url, api_key):
-    headers = {'Authorization': f'Bearer {api_key}'}
-    try:
-        response = requests.post(f'{instance_url}/api/v1/audit', headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching audit data from {instance_url}: {e}")
-        return None
+def check_access(instance_url, api_key):
+    headers = {
+        'content-type': 'application/json',
+        'X-N8N-API-KEY': api_key
+    }
+    final_url = f'{instance_url}/api/v1/executions?limit=1'
+    options = {
+        'method': 'GET',
+        'url': final_url,
+        'headers': headers
+    }
+    options['agentOptions'] = {
+        'rejectUnauthorized': False
+    }
+    response = requests.get(options['url'], headers=options['headers'])
+    return response.status_code < 400
     
-def get_users_from_n8n(instance_url, api_key):
-    headers = {'Authorization': f'Bearer {api_key}'}
-    try:
-        response = requests.get(f'{instance_url}/api/v1/users?limit=1000', headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching users from {instance_url}: {e}")
-        return None
+
+def push_data_to_n8nhackers(instance_name, type, raw):
+    data = {
+        'instance_name': instance_name,
+        'type': type,
+        'raw': raw
+    }
     
-def get_projects_from_n8n(instance_url, api_key):
-    headers = {'Authorization': f'Bearer {api_key}'}
+    headers = {
+        'X-API-KEY': N8NHACKERS_API_KEY,
+        'Content-Type': 'application/json'
+    }
     try:
-        response = requests.get(f'{instance_url}/api/v1/projects?limit=1000', headers=headers)
+        response = requests.post(f'{N8NHACKERS_API_URL}/api/v1/agent/data', json=data, headers=headers)
         response.raise_for_status()
-        return response.json()
+        print(f"Data pushed successfully for {instance_name}")
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching projects from {instance_url}: {e}")
-        return None
-
-def get_tags_from_n8n(instance_url, api_key):
-    headers = {'Authorization': f'Bearer {api_key}'}
-    try:
-        response = requests.get(f'{instance_url}/api/v1/tags?limit=1000', headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching tags from {instance_url}: {e}")
-        return None
-
-# Function to get data from n8n instance
-def get_data_from_n8n(instance_url, api_key):
-    headers = {'Authorization': f'Bearer {api_key}'}
-    try:
-        response = requests.get(f'{instance_url}/your-api-endpoint', headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data from {instance_url}: {e}")
-        return None
-
-# Function to push data to api.n8nhackers.com
-def push_data_to_api(data):
-    headers = {'Authorization': f'Bearer {API_KEY}'}
-    try:
-        response = requests.post('https://api-dev.n8nhackers.com/your-api-endpoint', json=data, headers=headers)
-        response.raise_for_status()
-        print("Data pushed successfully")
-    except requests.exceptions.RequestException as e:
-        print(f"Error pushing data to API: {e}")
-
+        print(f"Error pushing data for {instance_name}: {e}")
+    
+    
 # Function that fetches data from each n8n instance and pushes it to the API
-def task():
-    for instance in INSTANCES:
-        # print (f"Processing instance: {instance}")
-        instance_name, instance_url, instance_api_key = instance.split(',')
+def do_task(type):
+    for instance_name, instance_url, instance_api_key in INSTANCES:
         print(f"Fetching data from {instance_name}...")
-        # data = get_data_from_n8n(instance_url, instance_api_key)
-        # if data:
-        #     push_data_to_api(data)
+        if check_up(instance_url, instance_api_key):
+            if check_access(instance_url, instance_api_key):
+                if type == 'alarms':
+                    data = get_executions_from_n8n(instance_url, instance_api_key)
+                elif type == 'backups':
+                    data = get_workflows_from_n8n(instance_url, instance_api_key)
+                else:
+                    print(f"Unknown type: {type}")
+                    return
+                    
+                if data:
+                    push_data_to_n8nhackers(instance_name, type, data)
+            else:
+                print(f"Access denied for {instance_name}")
+        else:
+            print(f"{instance_name} is down")
+   
 
 # Schedule the task every x minutes (e.g., every 10 minutes)
-print ("Scheduling task to run every 1 minute...")
-schedule.every(1).minutes.do(task)
+print("Scheduling task to run every 1 minute...")
+# schedule.every(5).minutes.do(lambda: do_task('alarms'))
+# schedule.every(1).day.at("06:00").do(lambda: do_task('backups'))
 
-task()
+do_task('alarms')  # Run immediately for testing
 
 # Run the scheduler
 while True:
